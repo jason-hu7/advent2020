ca35d4a03868abfd8d577cb2247b4fd421940ea3
jason-hu7 <dcjasonhu@gmail.com>
2020-12-07 20:40:52 -0600

    day 7 part2

--- day7_handy_haversacks.py
+++ day7_handy_haversacks.py
@@ -25,6 +25,14 @@ def contain_target_color(contained_graph, target_color):
     return len(all_contains)
 
 
+def target_color_contains(contains_graph, target_color):
+    total_bags = 0
+    for cnt, c in contains_graph[target_color]:
+        total_bags += cnt
+        total_bags += cnt * target_color_contains(contains_graph, c)
+    return total_bags
+
+
 if __name__ == "__main__":
 
     """
@@ -35,8 +43,15 @@ if __name__ == "__main__":
     test1 = read_day7_inputs("inputs/day7_test.txt")
     contains, contained_in = build_graphs(test1)
     assert contain_target_color(contained_in, "shiny gold") == 4
+    test2 = read_day7_inputs("inputs/day7_test2.txt")
+    contains, contained_in = build_graphs(test2)
+    print(target_color_contains(contains, "shiny gold"))
+    assert target_color_contains(contains, "shiny gold") == 126
 
     # part1
     color_rules = read_day7_inputs("inputs/day7.txt")
     contains, contained_in = build_graphs(color_rules)
     print("part 1: ", contain_target_color(contained_in, "shiny gold"))
+
+    # part2
+    print("part 2: ", target_color_contains(contains, "shiny gold"))
